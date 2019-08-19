76362ffe5f109ee052821326923a29afc4bfd015
Santhosh Kondakindi <46731630+santhumax@users.noreply.github.com>
2019-08-19 14:18:02 +0100

    Update Hook.java

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -24,7 +24,7 @@ public class Hook extends BaseUtil{
 
         scenarioDef = base.features.createNode(scenario.getName());
 
-        System.out.println("Opening the browser : Firefox");
+        System.out.println("Opening the browser : Chrome");
 
         /*System.setProperty("webdriver.firefox.marionette", "D:\\Libs\\geckodriver.exe");
         base.Driver = new FirefoxDriver();*/
